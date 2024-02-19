caf2ed8b1000887107312ad7cde1543a7b065722
unknown <aniamilton129@gmail.com>
2024-02-19 15:48:53 +0200

    got the task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HTTPUtils;
+using System.Text.Json;
+using AnsiTools;
+using Colors = AnsiTools.ANSICodes.Colors;
+
+Console.Clear();
+Console.WriteLine("Starting Assignment 2");
+
+// SETUP 
+const string myPersonalID = "671d17d680b222051ca8593eb0cf836d57df720db5d08e026316d0c72fb753b9";
+const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
+const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
+
+// Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
+HttpUtils httpUtils = HttpUtils.instance;
+
+//#### REGISTRATION
+// We start by registering and getting the first task
+Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
+Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
+string taskID = "rEu25ZX"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+
+//#### FIRST TASK 
+// Fetch the details of the task from the server.
+Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task1 = JsonSerializer.Deserialize<Task>(task1Response.content);
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task1?.title}{ANSICodes.Reset}\n{task1?.description}\nParameters: {Colors.Yellow}{task1?.parameters}{ANSICodes.Reset}");
+
+// Calculate the answer to the task
+var answer = task1?.parameters.Split(",").Select(p => int.Parse(p.Trim())).Aggregate<int, int>(0, (acc, n) => acc + n);
+
+// Send the answer to the server
+Response task1AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer.ToString());
+Console.WriteLine($"Answer: {Colors.Green}{task1AnswerResponse}{ANSICodes.Reset}");
+
+taskID = "bbb"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+
+Console.WriteLine("\n-----------------------------------\n");
+
+//#### SECOND TASK
+// Fetch the details of the task from the server.
+Response task2Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task2 = JsonSerializer.Deserialize<Task>(task2Response.content);
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task2?.title}{ANSICodes.Reset}\n{task2?.description}\nParameters: {Colors.Yellow}{task2?.parameters}{ANSICodes.Reset}");
+
+
+var answer2 = task2.parameters.Contains("ice cream") ? "I scream for ice cream" : "I will have a cup of tea";
+// Send the answer to the server
+Response task2AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer2);
+Console.WriteLine($"\nAnswer: {Colors.Green}{task2AnswerResponse}{ANSICodes.Reset}");
+
+// That was the final task for the demo, so we are done here.
+
+class Task
+{
+    public string? title { get; set; }
+    public string? description { get; set; }
+    public string? taskID { get; set; }
+    public string? usierID { get; set; }
+    public string? parameters { get; set; }
+}
