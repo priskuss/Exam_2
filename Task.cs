9348d6739d3ae44dd56bfab4ddecd2f1c082c61f
unknown <aniamilton129@gmail.com>
2024-02-21 16:54:58 +0200

    added separate file for tasks

---
+++ Task.cs
@@ -0,0 +1,17 @@
+namespace TaskManager
+{
+    public class Task
+    {
+        public string? title { get; set; }
+        public string? description { get; set; }
+        public string? taskID { get; set; }
+        public string? usierID { get; set; }
+        public string? parameters { get; set; }
+
+    // Constants
+    public const string TASK_ID_ONE = "rEu25ZX";
+    public const string TASK_ID_TWO = "KO1pD3";
+    public const string TASK_ID_THREE = "aLp96";
+    public const string TASK_ID_FOUR = "otYK2";
+}
+}
