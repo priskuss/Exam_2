238d3b57fd47ce4ca52421012ce629d00e9c7193
unknown <aniamilton129@gmail.com>
2024-02-23 23:15:14 +0200

    refactored the main file

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ Console.Clear();
 Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Red}{Constants.STARTING_ASSIGNMENT}{ANSICodes.Reset}");
 
 // SETUP 
-const string myPersonalID = "671d17d680b222051ca8593eb0cf836d57df720db5d08e026316d0c72fb753b9";
-const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+const string myPersonalID = Constants.MY_PERSONAL_ID;
+const string baseURL = Constants.baseURL;
 const string startEndpoint = Constants.START; // baseURl + startEndpoint + myPersonalID
 const string taskEndpoint = Constants.TASK;   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
 
@@ -17,21 +17,25 @@ HttpUtils httpUtils = HttpUtils.instance;
 
 //#### FIRST TASK 
 // Fetch the details of the task from the server.
-TaskManager.Task task1 = new TaskManager.Task();
-task1.taskID = TaskManager.Task.TASK_ID_ONE;
-Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + task1.taskID); // Get the task from the server
-task1 = JsonSerializer.Deserialize<TaskManager.Task>(task1Response.content);
+TaskManager.Task FirstTask = new TaskManager.Task
+{
+    taskID = TaskManager.Task.TASK_ID_ONE
+};
+
+Response FirstTaskResponse = await httpUtils.Get($"{baseURL}{taskEndpoint}{myPersonalID}/{FirstTask.taskID}"); // Get the task from the server
+FirstTask = JsonSerializer.Deserialize<TaskManager.Task>(FirstTaskResponse.content);
 
 // Calculate the answer to the task
-string RomanNumber = task1?.parameters;
+string RomanNumber = FirstTask?.parameters;
 int answerTaskOne = ConvertRomanToInt(RomanNumber);
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_1}{task1.title}{ANSICodes.Reset}");
-Console.WriteLine($"{Colors.Blue}{task1.description}{ANSICodes.Reset}");
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{task1.parameters}{ANSICodes.Reset}");
+
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_1}{FirstTask.title}{ANSICodes.Reset}");
+Console.WriteLine($"{Colors.Blue}{FirstTask.description}{ANSICodes.Reset}");
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{FirstTask.parameters}{ANSICodes.Reset}");
 
 static int ConvertRomanToInt(string s)
 {
-    Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+    Dictionary<char, int> RomanMap = new Dictionary<char, int>
     {
         { 'I', 1 },
         { 'V', 5 },
@@ -57,26 +61,27 @@ static int ConvertRomanToInt(string s)
 }
 
 // Send the answer to the server
-Response task1AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + task1.taskID, answerTaskOne.ToString());
+Response FirstTaskAnswerResponse = await httpUtils.Post($"{baseURL}{taskEndpoint}{myPersonalID}/{FirstTask.taskID}", answerTaskOne.ToString());
 Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.ANSWER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{answerTaskOne}{ANSICodes.Reset}");
-//Console.WriteLine($"{Constants.RESPONSE}{Colors.Magenta}{task1AnswerResponse.content}{ANSICodes.Reset}");
 
-Console.WriteLine(Constants.CONSOLE_DIVIDER);
-
+ConsoleUtils.PrintColorfulDivider();
 
 //#### SECOND TASK 
 // Fetch the details of the task from the server.
-TaskManager.Task task2 = new TaskManager.Task();
-task2.taskID = TaskManager.Task.TASK_ID_TWO;
-Response task2Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + task2.taskID); // Get the task from the server
-task2 = JsonSerializer.Deserialize<TaskManager.Task>(task2Response.content);
+TaskManager.Task SecondTask = new TaskManager.Task
+{
+    taskID = TaskManager.Task.TASK_ID_TWO
+};
 
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_2}{task2.title}{ANSICodes.Reset}");
-Console.WriteLine($"{Colors.Blue}{task2.description}{ANSICodes.Reset}");
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{task2.parameters}{ANSICodes.Reset}");
+Response SecondTaskResponse = await httpUtils.Get($"{baseURL}{taskEndpoint}{myPersonalID}/{SecondTask.taskID}"); // Get the task from the server
+SecondTask = JsonSerializer.Deserialize<TaskManager.Task>(SecondTaskResponse.content);
+
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_2}{SecondTask.title}{ANSICodes.Reset}");
+Console.WriteLine($"{Colors.Blue}{SecondTask.description}{ANSICodes.Reset}");
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{SecondTask.parameters}{ANSICodes.Reset}");
 
 // Calculate the answer to the task 
-string[] parameters = task2.parameters.Split(',');
+string[] parameters = SecondTask.parameters.Split(',');
 int[] series = Array.ConvertAll(parameters, int.Parse);
 
 int difference = series[1] - series[0];
@@ -93,63 +98,64 @@ for (int i = 2; i < series.Length; i++)
 int answerTaskTwo = series[^1] + difference;
 
 // Send the answer to the server
-Response task2AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + task2.taskID, answerTaskTwo.ToString());
+Response SecondTaskAnswerResponse = await httpUtils.Post($"{baseURL}{taskEndpoint}{myPersonalID}/{SecondTask.taskID}", answerTaskTwo.ToString());
 Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.ANSWER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{answerTaskTwo}{ANSICodes.Reset}");
-//Console.WriteLine($"{Constants.RESPONSE}{Colors.Magenta}{task2AnswerResponse.content}{ANSICodes.Reset}");
 
-Console.WriteLine(Constants.CONSOLE_DIVIDER);
+ConsoleUtils.PrintColorfulDivider();
 
 //#### THIRD TASK 
 // Fetch the details of the task from the server.
-TaskManager.Task task3 = new TaskManager.Task();
-task3.taskID = TaskManager.Task.TASK_ID_THREE;
-Response task3Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + task3.taskID); // Get the task from the server
-task3 = JsonSerializer.Deserialize<TaskManager.Task>(task3Response.content);
+TaskManager.Task ThirdTask = new TaskManager.Task
+{
+    taskID = TaskManager.Task.TASK_ID_THREE
+};
 
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_3}{task3.title}{ANSICodes.Reset}");
-Console.WriteLine($"{Colors.Blue}{task3.description}{ANSICodes.Reset}");
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{task3.parameters}{ANSICodes.Reset}");
+Response ThirdTaskResponse = await httpUtils.Get($"{baseURL}{taskEndpoint}{myPersonalID}/{ThirdTask.taskID}"); // Get the task from the server
+ThirdTask = JsonSerializer.Deserialize<TaskManager.Task>(ThirdTaskResponse.content);
+
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_3}{ThirdTask.title}{ANSICodes.Reset}");
+Console.WriteLine($"{Colors.Blue}{ThirdTask.description}{ANSICodes.Reset}");
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{ThirdTask.parameters}{ANSICodes.Reset}");
 
 // Calculate the answer to the task
-int number = int.Parse(task3.parameters);
-string OddOrEven(int number)
+int number = int.Parse(ThirdTask.parameters);
+
+string IsOddOrEven(int number)
 {
-    if (number % 2 == 0)
-    {
-        return "even";
-    }
-    else
-    {
-        return "odd";
-    }
+    return number % 2 == 0 ? "even" : "odd";
 }
-string answerTaskThree = OddOrEven(number);
+
+string answerTaskThree = IsOddOrEven(number);
 
 // Send the answer to the server
-Response task3AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + task3.taskID, answerTaskThree);
+Response ThirdTaskAnswerResponse = await httpUtils.Post($"{baseURL}{taskEndpoint}{myPersonalID}/{ThirdTask.taskID}", answerTaskThree);
 Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.ANSWER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{answerTaskThree}{ANSICodes.Reset}");
-//Console.WriteLine($"{Constants.RESPONSE}{Colors.Magenta}{task3AnswerResponse.content}{ANSICodes.Reset}");
 
-Console.WriteLine(Constants.CONSOLE_DIVIDER);
+ConsoleUtils.PrintColorfulDivider();
 
-//#### FOURTH TASK
+//#### FOURTH TASK 
 // Fetch the details of the task from the server.
-TaskManager.Task task4 = new TaskManager.Task();
-task4.taskID = TaskManager.Task.TASK_ID_FOUR;
-Response task4Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + task4.taskID); // Get the task from the server
-task4 = JsonSerializer.Deserialize<TaskManager.Task>(task4Response.content);
+TaskManager.Task FourthTask = new TaskManager.Task
+{
+    taskID = TaskManager.Task.TASK_ID_FOUR
+};
 
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_4}{task4.title}{ANSICodes.Reset}");
-Console.WriteLine($"{Colors.Blue}{task4.description}{ANSICodes.Reset}");
-Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{task4.parameters}{ANSICodes.Reset}");
+Response FourthTaskResponse = await httpUtils.Get($"{baseURL}{taskEndpoint}{myPersonalID}/{FourthTask.taskID}"); // Get the task from the server
+FourthTask = JsonSerializer.Deserialize<TaskManager.Task>(FourthTaskResponse.content);
+
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Cyan}{Constants.TASK_4}{FourthTask.title}{ANSICodes.Reset}");
+Console.WriteLine($"{Colors.Blue}{FourthTask.description}{ANSICodes.Reset}");
+Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.PARAMETER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{FourthTask.parameters}{ANSICodes.Reset}");
 
 // Calculate the answer to the task
-string[] words = task4.parameters.Split(',');
+string[] words = FourthTask.parameters.Split(',');
 string[] uniqueWords = words.Distinct().OrderBy(word => word).ToArray();
 string answerTaskFour = string.Join(",", uniqueWords);
 
 // Send the answer to the server
-Response task4AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + task4.taskID, answerTaskFour);
+Response FourthTaskAnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + FourthTask.taskID, answerTaskFour);
 Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Yellow}{Constants.ANSWER}{ANSICodes.Reset}{ANSICodes.Effects.Bold}{Colors.Green}{answerTaskFour}{ANSICodes.Reset}");
+
+ConsoleUtils.PrintColorfulDivider();
+
 Console.WriteLine($"{ANSICodes.Effects.Bold}{Colors.Red}{Constants.FINAL_MESSAGE}{ANSICodes.Reset}");
-//Console.WriteLine($"{Constants.RESPONSE}{Colors.Magenta}{task4AnswerResponse.content}{ANSICodes.Reset}");
