d8d1168d669af8fcc2d353aa112c79c102fcb488
unknown <aniamilton129@gmail.com>
2024-02-23 23:17:22 +0200

    renamed the file and updated

---
+++ MagicStringsStorage.cs
@@ -0,0 +1,17 @@
+public static class Constants
+{
+    public const string STARTING_ASSIGNMENT = "\nStarting Assignment 2\n";
+    public const string MY_PERSONAL_ID = "671d17d680b222051ca8593eb0cf836d57df720db5d08e026316d0c72fb753b9";
+    public const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+    public const string TASK_1 = "Task 1: ";
+    public const string TASK_2 = "Task 2: ";
+    public const string TASK_3 = "Task 3: ";
+    public const string TASK_4 = "Task 4: ";
+    public const string ANSWER = "Answer: ";
+    public const string RESPONSE = "Response: ";
+    public const string PARAMETER = "Parameter: ";
+    public const string START = "start/";
+    public const string TASK = "task/";
+    public const string FINAL_MESSAGE = "\nAssignment 2 complete\n";
+
+}
